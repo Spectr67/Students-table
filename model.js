b5cce0ba5cc2ad8051f9329e17962c05c9359c6b
YouROCK <113207809+yourock1988@users.noreply.github.com>
2024-08-20 23:42:13 +0300

    prettier project

--- model.js
+++ model.js
@@ -1,18 +1,14 @@
 const API_STUDENTS = 'https://web-app.click/university/api/v1/students/'
-  let students = []
+let students = []
 
-  async function sendRequest(url) {
-    const resp = await fetch(url)
-    const json = await resp.json()
-    return json
-  }
+async function sendRequest(url) {
+  const resp = await fetch(url)
+  const json = await resp.json()
+  return json
+}
 
-  async function updateStudents() {
-    let obj = await sendRequest(API_STUDENTS)
-    students = obj.payload 
-    console.log (students)
-  }
-  
- 
-  
- 
+async function updateStudents() {
+  let obj = await sendRequest(API_STUDENTS)
+  students = obj.payload
+  console.log(students)
+}
--- view.js
+++ view.js
@@ -1,50 +1,44 @@
-const showButton = document.querySelector("button")
+const showButton = document.querySelector('button')
 showButton.onclick = onClickShowTable
 
 function onClickShowTable() {
   handleLoadStudents()
-} 
+}
 
 function renderTable(students) {
-students.forEach(renderTableRow)
+  students.forEach(renderTableRow)
 }
 
 function renderTableRow(student) {
-  const elTbody = document.querySelector("tbody")
+  const elTbody = document.querySelector('tbody')
   const elTr = generateTr(student)
   elTbody.appendChild(elTr)
 }
 
 function generateTr(student) {
-const elTr = document.createElement("tr")
-const elTdNone = document.createElement("th")
-const elTdId = document.createElement("th")
-const elTdFirstName = document.createElement("th")
-const elTdSecondName= document.createElement("th")
-const elTdAge = document.createElement("th")
-const elTdBudget = document.createElement("th")
-const elTdFaculty = document.createElement("th")
+  const elTr = document.createElement('tr')
+  const elTdNone = document.createElement('th')
+  const elTdId = document.createElement('th')
+  const elTdFirstName = document.createElement('th')
+  const elTdSecondName = document.createElement('th')
+  const elTdAge = document.createElement('th')
+  const elTdBudget = document.createElement('th')
+  const elTdFaculty = document.createElement('th')
 
-elTdId.textContent = student.id
-elTdFirstName.textContent = student.firstName
-elTdSecondName.textContent = student.secondName
-elTdAge.textContent = student.age
-elTdBudget.textContent = student.isOnBudget 
-elTdFaculty.textContent = student.faculty 
+  elTdId.textContent = student.id
+  elTdFirstName.textContent = student.firstName
+  elTdSecondName.textContent = student.secondName
+  elTdAge.textContent = student.age
+  elTdBudget.textContent = student.isOnBudget
+  elTdFaculty.textContent = student.faculty
 
-elTr.appendChild(elTdNone)
-elTr.appendChild(elTdId)
-elTr.appendChild(elTdFirstName)
-elTr.appendChild(elTdSecondName)
-elTr.appendChild(elTdAge)
-elTr.appendChild(elTdBudget)
-elTr.appendChild(elTdFaculty)
+  elTr.appendChild(elTdNone)
+  elTr.appendChild(elTdId)
+  elTr.appendChild(elTdFirstName)
+  elTr.appendChild(elTdSecondName)
+  elTr.appendChild(elTdAge)
+  elTr.appendChild(elTdBudget)
+  elTr.appendChild(elTdFaculty)
 
-
-return elTr
+  return elTr
 }
-
-
-
-
-// Object.keys(col).forEach(key => console.log('hello ' + key))
